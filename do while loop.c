763ede0bbc3b2fc1438503d0cce01b0972ab480a
John Gathuna <johngathuna8@gmail.com>
2025-10-08 09:11:46 +0300

    Add files via upload

---
+++ do while loop.c
@@ -0,0 +1,24 @@
+/*
+Name:John Gathuna
+Reg No:CT100/G/22448/24
+Description:
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+char password[20];
+
+do{
+	printf("Enter password:");	
+	scanf("%s,& password");
+}while(strcmp(password,"1234")!=0);
+
+printf("Access Granted\n");
+
+	
+
+	return 0;
+}
